bf2f891e44524a736026b70fa24ae9b64064a269
Claudio Bernasconi <email@claudio-bernasconi.net>
2021-05-21 16:41:06 +0200

    Add State Container

--- BlazorParentChildCom/Client/Program.cs
+++ BlazorParentChildCom/Client/Program.cs
@@ -1,11 +1,8 @@
+using BlazorParentChildCom.Client.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorParentChildCom.Client
@@ -18,6 +15,7 @@ namespace BlazorParentChildCom.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton<AppState>();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorParentChildCom/Client/Shared/AppState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlazorParentChildCom.Client.Shared
+{
+    public class AppState
+    {
+        public string FavoriteAnimal { get; set; }
+        
+        public event Action OnFavoriteAnimalChange;
+
+        public void SetFavoriteAnimal(string favoriteAnimal)
+        {
+            FavoriteAnimal = favoriteAnimal;
+            OnFavoriteAnimalChange?.Invoke();
+        }
+    }
+}
